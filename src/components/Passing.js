660f2eec95a40f0d2c9c797777d053f433bb6c08
Boris Gelder <bmgelder@arcor.de>
2021-01-12 22:21:02 +0100

    Translate to German 2

--- src/components/Passing.js
+++ src/components/Passing.js
@@ -7,7 +7,7 @@ const formatter = formatNumber();
 export default function Passing({ data }) {
   return data.map((_, i) => (
     <p key={i}>
-      Misses {_.orbiting_body} tomorrow at{' '}
+      Daneben {_.orbiting_body} morgen um{' '}
       {format(_.epoch_date_close_approach, 'h:mmaaaa')} by{' '}
       {formatter(parseInt(_.miss_distance.miles, 10))} miles whilst travelling
       at {formatNumber({ truncate: 0 })(_.relative_velocity.miles_per_hour)}mph
